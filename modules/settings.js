5ff4a2edf1b1371cd5d7d4385702d60b065abf4d
mclemente <matheus.xb@gmail.com>
2022-01-31 12:40:09 -0300

    Update settings.js

--- modules/settings.js
+++ modules/settings.js
@@ -1,12 +1,3 @@
 import constants from "./constants.mjs";
 
-export default function registerSettings() {
-	game.settings.register(constants.moduleName, "playersWelcomeScreen", {
-		name: "EasyPolls.Settings.playersWelcomeScreen.Enable",
-		hint: "EasyPolls.Settings.playersWelcomeScreen.EnableHint",
-		scope: "world",
-		config: true,
-		default: true,
-		type: Boolean,
-	});
-}
+export default function registerSettings() {}
