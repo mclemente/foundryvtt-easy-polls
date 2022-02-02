687564eb28387327eb7a6ab88199e4e8feb1ba21
mclemente <matheus.xb@gmail.com>
2022-02-02 12:06:29 -0300

    Localization changes

--- modules/utility/QuickPolls.js
+++ modules/utility/QuickPolls.js
@@ -2,6 +2,6 @@ export const QuickPolls = {};
 
 QuickPolls.yn = {
 	name: "EasyPolls.QuickPolls.yn.name",
-	parts: ["EasyPolls.QuickPolls.yn.Yes", "EasyPolls.QuickPolls.yn.No"],
+	parts: ["Yes", "No"],
 	question: "EasyPolls.QuickPolls.Poll",
 };
