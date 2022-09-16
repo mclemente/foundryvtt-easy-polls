a70e66a42bac18384651dc678aafcf277a516269
mclemente <matheus.xb@gmail.com>
2022-09-16 12:08:15 -0300

    Update settings.js

--- modules/settings.js
+++ modules/settings.js
@@ -1,3 +1,13 @@
 import constants from "./constants.mjs";
 
+function addSetting(key, data) {
+	const commonData = {
+		name: `EasyPolls.Settings.${key}.name`,
+		hint: `EasyPolls.Settings.${key}.hint`,
+		scope: "world",
+		config: true,
+	};
+	game.settings.register(constants.moduleName, key, Object.assign(commonData, data));
+}
+
 export default function registerSettings() {}
